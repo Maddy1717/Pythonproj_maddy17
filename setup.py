cf0d5e7cbd12f25d12a140f764e3733e3fc410d2
Maddy1717 <121928217+Maddy1717@users.noreply.github.com>
2023-01-04 15:38:12 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='Pythonproj_maddy17',
+    version='0.1.0',    
+    description='A example Python package',
+    author='Maathavan Manimaran',
+    author_email='maathavan17@gmail.com',
+    url = https://github.com/Maddy1717/Pythonproj_maddy17.git
+    license='MIT',
+    packages=['Pythonproj_maddy17'],
+ 
+    
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+)
+
+
